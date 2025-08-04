7711d817794ecf42706b6035325e7fbe3e94a89d
IVKovalev <121474420+IVKovalev@users.noreply.github.com>
2025-08-04 13:51:57 +1200

    Update app.py

--- app.py
+++ app.py
@@ -80,12 +80,10 @@ def save_report_changes():
     return redirect('/report')
 
 # Создание таблиц при первом запуске (или вручную в консоли)
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
