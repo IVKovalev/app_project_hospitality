f3969019158d8dff1955a68c71946a3ea477c435
IVKovalev <121474420+IVKovalev@users.noreply.github.com>
2025-08-04 13:19:05 +1200

    Update app.py

--- app.py
+++ app.py
@@ -80,9 +80,11 @@ def save_report_changes():
     return redirect('/report')
 
 # Создание таблиц при первом запуске (или вручную в консоли)
-@app.before_first_request
-def create_tables():
-    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
